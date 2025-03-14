f62336d2244dad1fb966cb15f0e1835735ccffd0
prince <princeessandoh316@gmail.com>
2025-03-14 01:03:51 +0000

    f

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -421,7 +421,7 @@ const handleStartPointClick = (location: Location) => {
           border: '1px solid rgba(0,0,0,0.1)',
           margin: isMobile ? '16px auto' : '16px 16px 16px 0',
           position : 'fixed',
-          bottom: isMobile ? (inputFocused ? '20%' : '-60%') : '',
+          bottom: isMobile ? (inputFocused ? '0%' : '-60%') : '',
           transition: 'bottom 0.3s ease-in-out',
           // transition: isDragging ? 'none' : 'bottom 0.3s ease-in-out',
           // bottom: isMobile ? `${drawerPosition}px` : '' 
