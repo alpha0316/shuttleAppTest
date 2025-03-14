602490df78b6b0be6f5ccc842a9e976f3e604226
prince <princeessandoh316@gmail.com>
2025-03-14 21:39:19 +0000

    f

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -453,7 +453,7 @@ const LocationList: React.FC<LocationListProps> = ({
           border: '1px solid rgba(0,0,0,0.1)',
           margin: isMobile ? '16px auto' : '16px 16px 16px 0',
           position : 'fixed',
-          bottom: isMobile ? (pickUp ? '0%' : '65%') : '',
+          bottom: isMobile ? (pickUp ? '0%' : '95%') : '',
           transition: 'bottom 0.3s ease-in-out',
           // transition: isDragging ? 'none' : 'bottom 0.3s ease-in-out',
           // bottom: isMobile ? `${drawerPosition}px` : '' 
