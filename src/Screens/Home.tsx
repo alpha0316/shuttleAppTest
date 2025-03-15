a13cf052d28c4f356a7f5ef6dfed7238111b9cdc
prince <princeessandoh316@gmail.com>
2025-03-15 11:25:32 +0000

    f

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -444,7 +444,7 @@ const LocationList: React.FC<LocationListProps> = ({
           position : 'fixed',
           bottom: isMobile 
           ? (dropDown 
-            ? '-125%' 
+            ? '-95%' 
             : pickUp 
               ? '-10%'  
               : '-15%'
