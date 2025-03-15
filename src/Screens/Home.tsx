5aecb8fc327b8a391557d0ade569c59235a0df3c
prince <princeessandoh316@gmail.com>
2025-03-15 11:29:31 +0000

    q

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -447,7 +447,7 @@ const LocationList: React.FC<LocationListProps> = ({
             ? '-55%' 
             : pickUp 
               ? '-10%'  
-              : '-15%'
+              : '-35%'
           ) 
           : '',
           // -55
